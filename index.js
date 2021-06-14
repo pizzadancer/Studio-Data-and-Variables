1864e2aa81a839c1027ce48ec163f8171425719f
dlm <55953676+pizzadancer@users.noreply.github.com>
2021-06-14 20:20:18 +0000

    Added input for astronautCount

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // FORK this starter file and save it to your own Repl.it account.
-
+const input = require('readline-sync');
 
 // Declare and initialize the 12 variables here:
 let date = "Monday 2019-03-18";
 let time = "10:04:34 AM";
-let astronautCount = 7;
+let astronautCount = Number(input.question("How many astronauts on board today? "));
 let astronautStatus = "ready";
 let averageAstronautMassKg = 80.7;
 let crewMassKg = astronautCount * averageAstronautMassKg;
@@ -12,14 +12,58 @@ let fuelMassKg = 760000;
 let shuttleMassKg = 74842.31;
 let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
 let fuelTempCelsius = -225;
-let fuelLevel = .01;
+let fuelLevel = 100;
 let weatherStatus = "clear";
-
+let clearForTakeoff = true;
 
 
 
 // Write code to generate the LC04 report here:
 let dashes = "-------------------------------------";
+
+console.log(dashes);
+console.log("> LC04 - LAUNCH CHECKLIST");
+console.log(dashes);
+console.log(`Date: ${date}`);
+console.log(`Time: ${time}`);
+console.log()
+
+console.log(dashes);
+console.log("> ASTRONAUT INFO");
+console.log(dashes);
+console.log(`* count: ${astronautCount}`);
+console.log(`* status: ${astronautStatus}`);
+console.log()
+
+console.log(dashes);
+console.log("> FUEL DATA");
+console.log(dashes);
+console.log(`* Fuel temp celsius: ${fuelTempCelsius}`);
+console.log(`* Fuel level: ${fuelLevel}%`);
+console.log();
+
+console.log(dashes);
+console.log("> MASS DATA");
+console.log(dashes);
+console.log(`* Crew mass: ${crewMassKg} kg`);
+console.log(`* Fuel mass: ${fuelMassKg} kg`);
+console.log(`* Shuttle mass: ${shuttleMassKg} kg`);
+console.log(`* Total mass: ${totalMassKg} kg`);
+console.log()
+
+console.log(dashes);
+console.log("> FLIGHT PLAN");
+console.log(dashes);
+console.log(`* weather: ${weatherStatus}`);
+console.log()
+
+console.log(dashes);
+console.log("> OVERALL STATUS");
+console.log(dashes);
+if (clearForTakeoff === true){
+  console.log(`* Clear for takeoff: YES`);
+}
+console.log()
 
 
 
